2c938487cac8bd952159b7244f7e9d14cb61daaf
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-02-13 11:37:20 +0044

    Context API with hooks

--- 04_react_hooks/src/views/examples/UseContext.jsx
+++ 04_react_hooks/src/views/examples/UseContext.jsx
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 import DataContext from '../../data/DataContext'
+import { AppContext } from '../../data/Store'
 
 const UseContext = (props) => {
 
@@ -14,6 +16,14 @@ const UseContext = (props) => {
         })
     }
 
+    const { number, text, setText, setNumber } = useContext(AppContext)
+
+    useEffect(() => {
+        if(number > 1250){
+            setText("Eita!!!!")
+        }
+    }, [number])
+
     return (
         <div className="UseContext">
             <PageTitle
@@ -21,6 +31,7 @@ const UseContext = (props) => {
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
 
+            <SectionTitle title="Exercício #01" />
             <div className="center">
                 <span className="text">{state.text}</span>
                 <span className="text">{state.number}</span>
@@ -31,6 +42,18 @@ const UseContext = (props) => {
                 </div>
             </div>
 
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <span className='text'>{text}</span>
+
+                <span className='text'>{number}</span>
+
+                <div>
+                    <button className="btn" onClick={()=>setNumber(number +1)}>+1</button>
+                    
+                    <button className="btn" onClick={()=>setNumber(number -1)}>-1</button>
+                </div>
+            </div>
         </div>
     )
 }
--- 04_react_hooks/src/views/examples/UseReducer.jsx
+++ 04_react_hooks/src/views/examples/UseReducer.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseReducer = (props) => {
+const UseReducer = () => {
+
+    const initialState = {
+        cart: [],
+        products: [],
+        user: null,
+        number: 0,
+    }
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'addNumber2':
+                return { ...state, number: state.number + 2 }
+            case 'numberMult7':
+                return { ...state, number: state.number * 7 }
+            case 'numberDiv25':
+                return { ...state, number: state.number / 25 }
+            case 'numberInt':
+                return { ...state, number: parseInt(state.number)  }
+            case 'numberAddN':
+                return { ...state, number: state.number +  action.payload  }
+            case 'login':
+                return { ...state, user: { name: action.payload } }
+            default:
+                return state
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+
+            <div className="center">
+                {state.user ? (
+                    <span className="text">{state.user.name}</span>
+                ) : (
+                    <span className="text">Login</span>
+                )}
+
+                <span className="text">{state.number}</span>
+                <div>
+                    <button className="btn" onClick={() => dispatch({ type: 'login', payload: 'Karla' })}>Login</button>
+
+                    <button className="btn" onClick={() => dispatch({ type: 'addNumber2' })}>+2</button>
+
+                    <button className="btn" onClick={() => dispatch({ type: 'numberMult7' })}>*7</button>
+
+                    <button className="btn" onClick={() => dispatch({ type: 'numberDiv25' })}>/25</button>
+
+                    <button className="btn" onClick={() => dispatch({ type: 'numberInt' })}>Int</button>
+
+                    <button className="btn" onClick={() => dispatch({ type: 'numberAddN', payload: 10 })}>+10</button>
+                </div>
+            </div>
         </div>
     )
 }
