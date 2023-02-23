9f447d2eb973350ef0fa5c52f8ca23b6c8366080
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-02-23 12:03:12 -0300

    Js fundamental

---
+++ 12_js/ex14.js
@@ -0,0 +1,9 @@
+const carrinho = [
+    '{"nome": "borracha", "preco": 3.45}',
+    '{"nome": "caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lápis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}',
+]
+
+const resultado = carrinho.map(p => JSON.parse(p)).map(p => p.preco)
+console.log(resultado)
